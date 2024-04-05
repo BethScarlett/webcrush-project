0eed72a72d731e3afa1bd486e0dda1efda00986e
Bethany Burrows <jedcburrows@gmail.com>
2024-04-05 17:06:03 +0100

    Added placeholder elements in the index.html along with basic styling to make them visible. Begun work in getting player to move position when an arrow is clicked. Finally wrote out work to be completed in ToDo file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 import "./Styling/main.scss";
+
+const player = document.querySelector<HTMLImageElement>(
+  ".game-display__player"
+);
+const gameDisplay = document.querySelector(".game-display");
+const arrowButton = document.querySelector(".game-controls__arrows--arrow");
+
+if (!arrowButton || !gameDisplay) {
+  throw new Error("Error with arrows");
+}
+
+if (!player) {
+  throw new Error("Error with player");
+}
+
+const handlePlayerMove = () => {
+  console.log("Moving");
+};
+
+arrowButton.addEventListener("click", handlePlayerMove);
