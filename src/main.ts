58d0fd70226da1e49a26077f024e57c978c22819
Bethany Burrows <jedcburrows@gmail.com>
2024-04-06 15:00:58 +0100

    Completed movement functionality for all 4 directions, fixed related bugs and correctly rotated arrows on screen. Updated ToDo to remove completed work from 'To Be Completed'.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const player = document.querySelector<HTMLImageElement>(
   ".game-display__player"
 );
 const gameDisplay = document.querySelector<HTMLDivElement>(".game-display");
-const arrowButton = document.querySelector(".game-controls__arrows--arrow");
+const arrowButton = document.querySelectorAll(".game-controls__arrows--arrow");
 
 if (!arrowButton || !gameDisplay) {
   throw new Error("Error with arrows");
@@ -19,15 +19,44 @@ window.onload = function () {
   setInterval(function () {});
 };
 
-let movement: number = 10;
+let horizontalMovement: number = Number(player.style.left);
+let verticalMovement: number = Number(player.style.top);
 let moveCounter: number;
 
-const handlePlayerMove = () => {
+const handlePlayerMove = (event: Event) => {
+  const getArrow = event.target as HTMLImageElement;
+  const getId = getArrow.id;
+
+  gameDisplay.style.backgroundColor = "green";
+
   moveCounter = setInterval(() => {
-    let horizontal: string = `${movement.toString()}px`;
-    gameDisplay.style.backgroundColor = "green";
-    player.style.left = `${horizontal}`;
-    movement = movement + 30;
+    switch (getId) {
+      case "up": {
+        verticalMovement = verticalMovement - 30;
+        let vertical: string = `${verticalMovement.toString()}px`;
+        player.style.top = `${vertical}`;
+        break;
+      }
+      case "down": {
+        verticalMovement = verticalMovement + 30;
+        let vertical: string = `${verticalMovement.toString()}px`;
+        player.style.top = `${vertical}`;
+        break;
+      }
+      case "left": {
+        horizontalMovement = horizontalMovement - 30;
+        let horizontal: string = `${horizontalMovement.toString()}px`;
+        player.style.left = `${horizontal}`;
+        break;
+      }
+      case "right": {
+        horizontalMovement = horizontalMovement + 30;
+        let horizontal: string = `${horizontalMovement.toString()}px`;
+        console.log(horizontal);
+        player.style.left = `${horizontal}`;
+        break;
+      }
+    }
   }, 200);
 };
 
@@ -35,5 +64,8 @@ const handlePlayerStop = () => {
   clearInterval(moveCounter);
 };
 
-arrowButton.addEventListener("mousedown", handlePlayerMove);
-arrowButton.addEventListener("mouseup", handlePlayerStop);
+arrowButton.forEach((arrow) => {
+  arrow.addEventListener("mousedown", handlePlayerMove);
+  arrow.addEventListener("mouseup", handlePlayerStop);
+  arrow.addEventListener("mouseout", handlePlayerStop);
+});
