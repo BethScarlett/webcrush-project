c8c4fbec80f684c8d2aaab0566767c042ac91eec
Bethany Burrows <jedcburrows@gmail.com>
2024-04-06 13:52:41 +0100

    Created functionality which allows the player to move while arrow is held down.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "./Styling/main.scss";
 const player = document.querySelector<HTMLImageElement>(
   ".game-display__player"
 );
-const gameDisplay = document.querySelector(".game-display");
+const gameDisplay = document.querySelector<HTMLDivElement>(".game-display");
 const arrowButton = document.querySelector(".game-controls__arrows--arrow");
 
 if (!arrowButton || !gameDisplay) {
@@ -14,8 +14,26 @@ if (!player) {
   throw new Error("Error with player");
 }
 
-const handlePlayerMove = () => {
-  console.log("Moving");
+//TODO - Use on load to set game up and setInterval to keep track of score
+window.onload = function () {
+  setInterval(function () {});
 };
 
-arrowButton.addEventListener("click", handlePlayerMove);
+let movement: number = 10;
+let moveCounter: number;
+
+const handlePlayerMove = () => {
+  moveCounter = setInterval(() => {
+    let horizontal: string = `${movement.toString()}px`;
+    gameDisplay.style.backgroundColor = "green";
+    player.style.left = `${horizontal}`;
+    movement = movement + 30;
+  }, 200);
+};
+
+const handlePlayerStop = () => {
+  clearInterval(moveCounter);
+};
+
+arrowButton.addEventListener("mousedown", handlePlayerMove);
+arrowButton.addEventListener("mouseup", handlePlayerStop);
