967adaa2b0b12ce7f7417f7f82038547cf641339
Bethany Burrows <jedcburrows@gmail.com>
2024-04-09 15:15:18 +0100

    Created collision detection between player and gems, then incremented score and removed collided gem on collision. Also created player type and playerOne object to house score and speed.

---
+++ src/Types & Objects/types.ts
@@ -0,0 +1,4 @@
+export type Player = {
+  score: number;
+  moveSpeed: number;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,70 @@
 import "./Styling/main.scss";
+import playerOne from "./Types & Objects/objects";
 
-//Grab player, display and movement arrows from HTML
+//Grab player, display, gem and movement arrows from HTML
 const player = document.querySelector<HTMLImageElement>(
   ".game-display__player"
 );
+const gems = document.querySelectorAll<HTMLImageElement>(".game-display__gem");
 const gameDisplay = document.querySelector<HTMLDivElement>(".game-display");
 const arrowButton = document.querySelectorAll(".game-controls__arrows--arrow");
 
 //Null Exceptions for above query selectors
-if (!arrowButton || !gameDisplay || !player) {
+if (!arrowButton || !gameDisplay || !player || !gems) {
   throw new Error("Issue with selectors");
 }
 
 //TODO - Use on load to set game up and setInterval to keep track of score
-window.onload = function () {
-  setInterval(function () {});
-};
+// window.onload = function () {
+//   setInterval(function () {});
+// };
 
 //Set variables required for player movement
 let horizontalMovement: number = Number(player.style.left);
 let verticalMovement: number = Number(player.style.top);
 let moveCounter: number;
+
+//Function to check game state every 50ms
+setInterval(() => {
+  checkCollision();
+  checkGameEndConditions();
+}, 5000);
+
+//Functions to be checked every 50ms
+const checkCollision = () => {
+  //Gets the player position on x and y axis and add width + height respectively
+  const playerPosition: number[] = [
+    player.x + player.width,
+    player.y + player.height,
+  ];
+
+  //Check if each gem is colliding with the player
+  gems.forEach((gem) => {
+    const gemPosition: number[] = [gem.x + gem.width, gem.y + gem.height];
+    if (
+      player.x < gemPosition[0] &&
+      playerPosition[0] > gem.x &&
+      player.y < gemPosition[1] &&
+      playerPosition[1] > gem.y
+    ) {
+      gem.remove();
+      gem.width = 0;
+      gem.height = 0;
+      playerOne.score += 1;
+    }
+  });
+  console.log("Player score = " + playerOne.score);
+};
+
+const checkGameEndConditions = () => {
+  //Check if the players score has hit the goal
+  //Check if the time has elapsed
+};
+
+//TODO - Remove below console logs from code (keeping incase needed)
+//Get position data for player and gem
+// console.log(player.getBoundingClientRect());
+// console.log(gem.getBoundingClientRect());
 
 //Increment player movement based on arrow clicked
 const handlePlayerMove = (event: Event) => {
